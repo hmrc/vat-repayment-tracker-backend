145fa108c0ea46fd85201bf7a70bb5a53b80b62d
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2023-10-18 14:14:37 +0100

    [AC][OPS-10906] library updates (#19)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,29 +6,7 @@ object AppDependencies {
 
   val hmrcMongoVersion = "1.3.0"
   val bootstrapVersion = "7.22.0"
-  val jacksonVersion = "2.13.2"
-  val jacksonDatabindVersion = "2.13.2.2"
 
-  val jacksonOverrides = Seq(
-    // format: OFF
-    "com.fasterxml.jackson.core" % "jackson-core",
-    "com.fasterxml.jackson.core" % "jackson-annotations",
-    "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8",
-    "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310"
-    // format: ON
-  ).map(_ % jacksonVersion)
-
-  val jacksonDatabindOverrides = Seq(
-    "com.fasterxml.jackson.core" % "jackson-databind" % jacksonDatabindVersion
-  )
-
-  val akkaSerializationJacksonOverrides = Seq(
-    // format: OFF
-    "com.fasterxml.jackson.dataformat" % "jackson-dataformat-cbor",
-    "com.fasterxml.jackson.module" % "jackson-module-parameter-names",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"
-    // format: ON
-  ).map(_ % jacksonVersion)
 
   val compile =  {
     val dependencies = Seq(
@@ -36,7 +14,7 @@ object AppDependencies {
       "uk.gov.hmrc"             %% "bootstrap-backend-play-28"   % bootstrapVersion,
       "com.beachape"            %% "enumeratum-play"             % "1.7.2"
     )
-    dependencies ++ jacksonDatabindOverrides ++ jacksonOverrides ++ akkaSerializationJacksonOverrides
+    dependencies
   }
 
   val test = Seq(
@@ -46,7 +24,7 @@ object AppDependencies {
     "com.vladsch.flexmark"     % "flexmark-all"             % "0.64.6"                % "test, it",
     "com.typesafe.play"       %% "play-test"                % current                 % Test,
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"                 % Test,
-    "org.wiremock"             % "wiremock-standalone"      % "3.0.4"                % Test
+    "org.wiremock"             % "wiremock-standalone"      % "3.2.0"                 % Test
   )
 
 }
