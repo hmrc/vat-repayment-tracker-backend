93c514a87d4d98d55a02b6b93b34b458197f8169
Alexis Morcrette <26277447+almorcrette@users.noreply.github.com>
2023-10-30 10:51:26 +0000

    [AM][OPS-9115] inject execution context in start up clean up task, and excluded scalatestplus.play from updates

--- app/uk/gov/hmrc/vrt/tasks/TasksModule.scala
+++ app/uk/gov/hmrc/vrt/tasks/TasksModule.scala
@@ -5,12 +5,12 @@ import play.api.inject._
 import uk.gov.hmrc.mongo.MongoComponent
 
 import javax.inject.{Inject, Singleton}
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.ExecutionContext
 
 class TasksModule extends SimpleModule(bind[CleanupTask].toSelf.eagerly())
 
 @Singleton
-class CleanupTask @Inject() (mongoComponent: MongoComponent) extends Logging {
+class CleanupTask @Inject() (mongoComponent: MongoComponent)(implicit ec: ExecutionContext) extends Logging {
   logger.info("**************** Start cleanup tasks...")
 
   mongoComponent.client
