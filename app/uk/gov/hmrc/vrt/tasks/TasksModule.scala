77b22c473faa3f59102b20223edf8ca9ca489ae8
Alexis Morcrette <26277447+almorcrette@users.noreply.github.com>
2023-10-30 10:36:18 +0000

    [AM][OPS-9115] mongo migration clean up - drop legacy collection

---
+++ app/uk/gov/hmrc/vrt/tasks/TasksModule.scala
@@ -0,0 +1,22 @@
+package uk.gov.hmrc.vrt.tasks
+
+import play.api.Logging
+import play.api.inject._
+import uk.gov.hmrc.mongo.MongoComponent
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class TasksModule extends SimpleModule(bind[CleanupTask].toSelf.eagerly())
+
+@Singleton
+class CleanupTask @Inject() (mongoComponent: MongoComponent) extends Logging {
+  logger.info("**************** Start cleanup tasks...")
+
+  mongoComponent.client
+    .getDatabase("vat-repayment-tracker-backend") // update
+    .getCollection("repayment-details") // update
+    .drop()
+    .toFuture()
+    .map { _ => logger.info("**************** cleanup done.") }
+}
