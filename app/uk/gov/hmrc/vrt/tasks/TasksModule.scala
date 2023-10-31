a6472aeaec563f13ef62de9519f31adf2613061a
Alexis Morcrette <26277447+almorcrette@users.noreply.github.com>
2023-10-31 17:00:35 +0000

    [AM][OPS-9115] rename cache collection, transfer documents, drop old name, on start up + fix TTL (#22)

--- app/uk/gov/hmrc/vrt/tasks/TasksModule.scala
+++ app/uk/gov/hmrc/vrt/tasks/TasksModule.scala
@@ -1,5 +1,23 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.vrt.tasks
 
+import org.mongodb.scala.MongoNamespace
+import org.mongodb.scala.model.RenameCollectionOptions
 import play.api.Logging
 import play.api.inject._
 import uk.gov.hmrc.mongo.MongoComponent
@@ -7,16 +25,19 @@ import uk.gov.hmrc.mongo.MongoComponent
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.ExecutionContext
 
-class TasksModule extends SimpleModule(bind[CleanupTask].toSelf.eagerly())
+class TasksModule extends SimpleModule(bind[RenameCollectionTask].toSelf.eagerly())
 
 @Singleton
-class CleanupTask @Inject() (mongoComponent: MongoComponent)(implicit ec: ExecutionContext) extends Logging {
-  logger.info("**************** Start cleanup tasks...")
+class RenameCollectionTask @Inject() (mongoComponent: MongoComponent)(implicit ec: ExecutionContext) extends Logging {
+  logger.info("**************** Start collection rename task...")
 
   mongoComponent.client
-    .getDatabase("vat-repayment-tracker-backend") // update
-    .getCollection("repayment-details") // update
-    .drop()
+    .getDatabase("vat-repayment-tracker-backend")
+    .getCollection("repayment-details-new-mongo")
+    .renameCollection(
+      MongoNamespace("vat-repayment-tracker-backend", "repayment-details"),
+      RenameCollectionOptions().dropTarget(true)
+    )
     .toFuture()
-    .map { _ => logger.info("**************** cleanup done.") }
+    .map { _ => logger.info("**************** collection rename task done.") }
 }
