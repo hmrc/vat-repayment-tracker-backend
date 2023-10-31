6214e20c6354be64c047346800442152620da803
Alexis Morcrette <26277447+almorcrette@users.noreply.github.com>
2023-10-31 11:41:01 +0000

    [AM][OPS-9115] drop old collection before renaming it

--- app/gov/hmrc/vrt/tasks/TasksModule.scala
+++ app/gov/hmrc/vrt/tasks/TasksModule.scala
@@ -1,6 +1,8 @@
 package gov.hmrc.vrt.tasks
 
+import com.mongodb.client.model.RenameCollectionOptions
 import org.mongodb.scala.MongoNamespace
+import org.mongodb.scala.model.RenameCollectionOptions
 import play.api.Logging
 import play.api.inject._
 import uk.gov.hmrc.mongo.MongoComponent
@@ -17,6 +19,6 @@ class RenameCollectionTask @Inject()(mongoComponent: MongoComponent)(implicit ec
   mongoComponent.client
     .getDatabase("vat-repayment-tracker-backend")
     .getCollection("repayment-details-new-mongo")
-    .renameCollection(MongoNamespace("vat-repayment-tracker-backend", "repayment-details"))
+    .renameCollection(MongoNamespace("vat-repayment-tracker-backend", "repayment-details"), RenameCollectionOptions().dropTarget(true))
     .map { _ => logger.info("**************** collection rename task done.") }
 }
