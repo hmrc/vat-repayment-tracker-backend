bb46a0be6104ea52454d94bd47a6d64586973b3e
DarrenCB <58850786+DarrenCB@users.noreply.github.com>
2024-12-18 11:02:35 +0000

    [DCB] address comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,4 +1,3 @@
-import play.core.PlayVersion.current
 import sbt._
 
 
@@ -11,14 +10,12 @@ object AppDependencies {
   val compile = Seq(
       "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"          % hmrcMongoVersion,
       "uk.gov.hmrc"             %% "bootstrap-backend-play-30"   % bootstrapVersion,
-      "com.beachape"            %% "enumeratum-play"             % "1.8.0"
+      "com.beachape"            %% "enumeratum-play"             % "1.8.2"
     )
 
   val test = Seq(
-    "org.scalatest"           %% "scalatest"                % "3.2.19"        ,
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"   % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"  % hmrcMongoVersion,
-    "org.playframework"       %% "play-test"                % current         ,
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "7.0.1"         ,
     "org.wiremock"             % "wiremock-standalone"      % "3.10.0"
   ).map(_ % Test)
