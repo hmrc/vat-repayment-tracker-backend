47cffafdfe8b5c125a76585510f5027f9094158c
slavvco <13876870+slavvco@users.noreply.github.com>
2026-01-29 13:44:13 +0000

    OPS-14492 | Dependency update. (#42)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 
 object AppDependencies {
-  val hmrcMongoVersion = "2.11.0"
+  val hmrcMongoVersion = "2.12.0"
   val bootstrapVersion = "10.5.0"
 
   val compile: Seq[ModuleID] = Seq(
       "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"          % hmrcMongoVersion,
       "uk.gov.hmrc"             %% "bootstrap-backend-play-30"   % bootstrapVersion,
-      "com.beachape"            %% "enumeratum-play"             % "1.9.1"
+      "com.beachape"            %% "enumeratum-play"             % "1.9.2"
     )
 
   val test: Seq[ModuleID] = Seq(
